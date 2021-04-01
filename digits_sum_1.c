ab74891b98e460fdacb34ab879dd27e66daaf832
murnat98 <murnat98@yandex.ru>
2021-04-01 21:52:31 +0400

    hw examples

---
+++ digits_sum_1.c
@@ -0,0 +1,8 @@
+int main()
+{
+    int number = 354; //  Or another 3-digit number, you wish
+
+    // TODO: compute sum of sum of digits of variable `number` HERE, and then print it
+
+    return 0;
+}
---
+++ triangle_area_2.c
@@ -0,0 +1,9 @@
+int main()
+{
+    int leg_1 = 3; // leg 1 of the triangle
+    int leg_2 = 5; // leg 2 of the triangle
+
+    // TODO: compute area og the given triangle HERE, and print it 
+
+    return 0;
+}
